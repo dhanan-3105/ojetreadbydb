7432f6aff6cc1cbb01d09634de5630e7f6b67bff
DJay <40359449+dhanan77@users.noreply.github.com>
2022-12-16 16:34:47 +0530

    Update nodedb.js

--- dbnodeserv/nodedb.js
+++ dbnodeserv/nodedb.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.99.100:8000');  
 
    var con = mysql.createConnection({
-    host: "172.100.100.100",
+    host: "dbservice.demo.svc.cluster.local",
     user: "root",
     password: "admin",
     database: "pythonlogin"
